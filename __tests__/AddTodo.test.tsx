0b3739186bd6740ff0cc0bc4df8766198216a334
yacmeno <yacine.seffal@gmail.com>
2019-12-12 11:16:29 -0500

    AddTodo: Empty and non-empty input field handling
    Enzyme's wrapper.state() only works on class components.

---
+++ __tests__/AddTodo.test.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import { AddTodo } from "../src/components/App";
+
+describe("<AddTodo />", () => {
+	test("Should have invalid state on empty input", () => {
+		const onAdd = jest.fn();
+		const component = shallow(<AddTodo onAdd={onAdd} />);
+
+		component.find("input").simulate("change", { target: { value: "" } });
+		//@ts-ignore
+		expect(component.state().isInvalid).toBe(true);
+	});
+
+	test("Should have valid state on non-empty input", () => {
+		const onAdd = jest.fn();
+		const component = shallow(<AddTodo onAdd={onAdd} />);
+
+		component
+			.find("input")
+			.simulate("change", { target: { value: "Not empty" } });
+
+		//@ts-ignore
+		expect(component.state().isInvalid).toBe(false);
+	});
+
+	test("Should disallow adding on invalid state", () => {
+		const onAdd = jest.fn();
+		const component = shallow(<AddTodo onAdd={onAdd} />);
+
+		component.find("input").simulate("change", { target: { value: "" } });
+		component.find("input").simulate("keydown", { key: "Enter" });
+
+		expect(component.find("button").is("[disabled]")).toBe(true);
+		expect(onAdd).toBeCalledTimes(0);
+	});
+
+	test("Should handle valid submissions", () => {
+		const onAdd = jest.fn();
+		const component = shallow(<AddTodo onAdd={onAdd} />);
+
+		// Add a todo through the button
+		component
+			.find("input")
+			.simulate("change", { target: { value: "Not empty" } });
+		component.find("button").simulate("click");
+
+		//Assert reset of input field
+		//@ts-ignore
+		expect(component.state().currentTodo).toBe("");
+
+		// Add a todo through enter key
+		component
+			.find("input")
+			.simulate("change", { target: { value: "Not empty 2" } });
+		component.find("input").simulate("keydown", { key: "Enter" });
+
+		expect(onAdd).toBeCalledTimes(2);
+	});
+});
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,18 +14,87 @@ const TodoApp: React.FC = () => {
 		return;
 	}
 
+	function onAdd() {
+		return;
+	}
+
 	return (
 		<div className="app__container">
-			<AddTodo />
+			<AddTodo onAdd={onAdd} />
 			<Nav />
 			<TodoList todos={dummyTodos} clickHandler={clickHandler} />
 		</div>
 	);
 };
 
-const AddTodo: React.FC = () => {
-	return <div>Header</div>;
-};
+interface IAddTodoProps {
+	onAdd: () => void;
+}
+interface IAddTodoState {
+	currentId: number;
+	currentTodo: string;
+	isInvalid: boolean;
+}
+
+export class AddTodo extends React.Component<IAddTodoProps, IAddTodoState> {
+	readonly state: IAddTodoState = {
+		currentId: 0,
+		currentTodo: "",
+		isInvalid: true,
+	};
+
+	private onInput(e: React.ChangeEvent<HTMLInputElement>) {
+		const invalidInput = e.target.value.trim() === "";
+		this.setState({
+			currentTodo: e.target.value,
+			isInvalid: invalidInput ? true : false,
+		});
+	}
+
+	private handleReturnKey(e: React.KeyboardEvent<HTMLInputElement>) {
+		if (this.state.isInvalid) {
+			return;
+		}
+
+		if (e.key === "Enter") {
+			this.handleSubmit();
+		}
+	}
+
+	private handleSubmit() {
+		this.props.onAdd();
+		this.setState(prevState => {
+			return {
+				currentId: prevState.currentId + 1,
+				currentTodo: "",
+				isInvalid: true,
+			};
+		});
+	}
+
+	render() {
+		const { currentTodo, isInvalid } = this.state;
+
+		return (
+			<div className="todo__header">
+				<input
+					type="text"
+					onChange={this.onInput.bind(this)}
+					onKeyDown={this.handleReturnKey.bind(this)}
+					value={currentTodo}
+				/>
+				<button
+					type="button"
+					disabled={isInvalid}
+					onClick={() => this.handleSubmit()}
+				>
+					Add Todo
+				</button>
+			</div>
+		);
+	}
+}
+
 const Nav: React.FC = () => {
 	return <div>Nav</div>;
 };
